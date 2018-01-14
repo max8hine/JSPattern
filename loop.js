fbc980710e325733b958a3f858af17d9bb83b17f
maxshine <max8hine@gmail.com>
2018-01-14 16:39:21 +1300

    ad eslint

--- loop.js
+++ loop.js
@@ -13,43 +13,45 @@ for(let key in arr) { console.log(arr[key])} // 3, 5, 7, Hello
 arr.map(v=>{console.log('map: ', v)})
 console.log('____________');
 // ES6
-for(let val of arr) { console.log(val)} // 3, 5, 7 
+for (let val of arr) {
+  console.log(val);
+} // 3, 5, 7
 
-console.log('____________');
+console.log("____________");
 
-var trees = ['redwood', 'bay', 'cedar', 'oak', 'maple']
+var trees = ["redwood", "bay", "cedar", "oak", "maple"];
 //  index:       0        1       2       3        4
 console.log(
   0 in trees, // true
   4 in trees, // ture
   5 in trees // false
-)
-console.log('____________');
-
-
-
-
+);
+console.log("____________");
 
 let obj = {
-  wow: 'obj_wow',
-  hey: 'obj_hey'
+  wow: "obj_wow",
+  hey: "obj_hey"
 };
-for(let val of arr) {console.log(val);}
-console.log('____________');
+for (let val of arr) {
+  console.log(val);
+}
+console.log("____________");
 
-for(let key in obj) {console.log(key, obj[key]);}
-console.log('____________');
+for (let key in obj) {
+  console.log(key, obj[key]);
+}
+console.log("____________");
 
-for(let key in arr) {console.log(key);}
-console.log('____________');
+for (let key in arr) {
+  console.log(key);
+}
+console.log("____________");
 
-
-
-var gen = (function *(){
+var gen = (function*() {
   yield 1;
   yield 2;
   yield 3;
 })();
 
-var arobg = Array.from('1234');
+var arobg = Array.from("1234");
 console.log(arobg);
