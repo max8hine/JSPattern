9e1404836cbc21ea5443ad561f06ded2b33ed8a4
maxshine <max8hine@gmail.com>
2018-07-29 17:34:29 +1200

    add Object capture

---
+++ Object/defineProperty.js
@@ -0,0 +1,9 @@
+/**
+ * how to use javascript Object.defineProperty
+ * ==========================================
+ * https://stackoverflow.com/questions/18524652/how-to-use-javascript-object-defineproperty
+*/
+
+
+
+/* END */
